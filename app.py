abf1f7bc42af7a2871b7abc944c5b6c49ff07939
moonheesoo1997 <pado199735@gmail.com>
2023-02-05 21:06:44 +0900

    finding-1

--- app.py
+++ app.py
@@ -21,6 +21,13 @@ def search_get():
     search_list = list(db.search_box.find({}, {'_id': False}))
     return jsonify({'search_box': search_list})
 
+@app.route("/?", methods=["GET"])
+def finding_get():
+
+    finding_list = dataset[dataset['productName'].str.contains( 'searchs_give', na = False)]
+
+    print(finding_list)
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
